70da730f1ed1e555205679f743ac678d5e14db73
Takarada0202 <90257535+Takarada0202@users.noreply.github.com>
2022-08-02 18:15:34 +0900

    v-if/v-for

---
+++ src/components/CardUi.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <h2 v-if="menuTitle">
+    {{menuTitle}}
+  </h2>
+  <p v-else class="untitled">
+    notitle
+  </p>
+  <ul>
+    <li v-for="menu in menuCount" :key="menu">{{menu}}</li>
+  </ul>
+</div>
+
+
+
+</template>
+
+<script>
+export default {
+  props : {
+    menuTitle : String,
+    menuCount : Number
+  }
+}
+</script>
+
+<style>
+.custom_input {
+  padding: 20px;
+  font-size: 20px;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 20px 0;
+}
+</style>
